2a257c996e4b0c59ec67748b3f6875cff2d20208
Ayesha Siddiqa <90017368+ayesha-siddiqa20@users.noreply.github.com>
2022-05-12 14:01:30 -0400

    Update Odd.java

--- arraylist/Odd.java
+++ arraylist/Odd.java
@@ -10,7 +10,6 @@ public class Odd {
 	 * @return An array list of positive odds, less than n [1,3,5,7,...]
 	 */
 	public static ArrayList<Integer> odds(int n) {
-		// See https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
 		ArrayList<Integer> oddNumbers = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
 			if (i % 2 == 1) {
